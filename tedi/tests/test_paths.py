40de4f4d958c6a80277141fa86b2594e4f3108f4
Toby McLaughlin <toby@jarpy.net>
2018-02-14 15:27:39 +1100

    Fix pyconfig leakage between tests

--- tedi/tests/test_paths.py
+++ tedi/tests/test_paths.py
@@ -6,7 +6,6 @@ from ..paths import get_render_path, get_template_path, make_render_path
 
 
 def test_make_render_path():
-    pyconfig.reload(clear=True)
     src = Path(get_template_path()) / 'one' / 'two'
     dst = make_render_path(src)
     assert dst == Path(os.path.join(str(get_render_path()), 'one', 'two'))
