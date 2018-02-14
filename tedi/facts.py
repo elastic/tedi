65e200e063074cb5836450656ba739c43d94930e
Toby McLaughlin <toby@jarpy.net>
2018-02-14 16:59:00 +1100

    Render "facts" into tempates

--- tedi/facts.py
+++ tedi/facts.py
@@ -1,8 +1,11 @@
 class Facts(object):
     def __init__(self):
         self.facts = {}
-        self.facts['test_canary_fact'] = 'Cats can be right-pawed or left-pawed.'
-        self.facts['elastic_version'] = '6.2.0'
+        self.set('test_canary_fact', 'Cats can be right-pawed or left-pawed.')
+        self.set('elastic_version', '6.2.0')
+
+    def set(self, key, value):
+        self.facts[key] = value
 
     def get_all(self):
         return self.facts
