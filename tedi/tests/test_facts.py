b78bc2d35cd3f9246deed8ae2822d401c494af41
Toby McLaughlin <toby@jarpy.net>
2018-02-14 17:03:23 +1100

    Test Facts.set() method

--- tedi/tests/test_facts.py
+++ tedi/tests/test_facts.py
@@ -1,5 +1,13 @@
 from ..facts import Facts
 
 
-def test_fact_elastic_version():
+def test_set_method_creates_new_facts():
+    facts = Facts()
+    canary = 'princely_camel'
+    assert canary not in facts.get_all()
+    facts.set(canary, True)
+    assert canary in facts.get_all()
+
+
+def test_elastic_version_is_present():
     assert 'elastic_version' in Facts().get_all()
