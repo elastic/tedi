590a04d2fc906b984d74acc3975d45fdad1be3fd
Toby McLaughlin <toby@jarpy.net>
2018-02-22 08:50:17 +1100

    Support special "default" variant for images

--- tedi/paths.py
+++ tedi/paths.py
@@ -14,7 +14,7 @@ class Paths(object):
     def render_path(self):
         return Path(pyconfig.get('render_path', 'renders'))
 
-    def make_render_path(self, path_to_template_file, suffix=None):
+    def make_render_path(self, path_to_template_file, suffix='default'):
         """Given a Path in the template dir, return the matched Path in the render dir
 
         For example, if template_path is "input/tree/templates", the following
@@ -23,12 +23,12 @@ class Paths(object):
         input/tree/templates/README -> renders/README
         input/tree/templates/image/Dockerfile -> renders/image/Dockerfile
 
-        If suffix is specified, it is added like this:
+        If suffix is specified and is not "default", it is added like this:
 
         input/tree/templates/image/... -> input/tree/templates/image-suffix/...
         """
         depth_of_template_root_dir = len(self.template_path.parts)
         trailing_components = list(path_to_template_file.parts[depth_of_template_root_dir:])
-        if suffix and trailing_components:
+        if not suffix == 'default' and trailing_components:
             trailing_components[0] += '-' + suffix
         return self.render_path / Path(*trailing_components)
