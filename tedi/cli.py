0d80ab64f92adc0d2587f1be898fe516214e9dcd
Toby McLaughlin <toby@jarpy.net>
2018-07-18 15:41:17 +1000

    Update help text

--- tedi/cli.py
+++ tedi/cli.py
@@ -7,13 +7,12 @@ logger = getLogger(__name__)
 
 @click.group()
 def cli():
-    """A empty command group to create subcommands like 'tedi render'"""
     pass
 
 
 @cli.command()
 def render():
-    """Render the projects to static files"""
+    """Render build contexts (.tedi/*)."""
     logger.debug('render subcommand called from cli')
     commands.clean()
     commands.render()
@@ -22,7 +21,7 @@ def render():
 @cli.command()
 @click.option('--clean-assets', is_flag=True, help='Also remove assets.')
 def clean(clean_assets):
-    """Remove all rendered files and (optionally) downloaded assets"""
+    """Remove rendered files and downloaded assets."""
     logger.debug('clean subcommand called from cli')
     commands.clean(clean_assets)
 
@@ -30,7 +29,7 @@ def clean(clean_assets):
 @cli.command()
 @click.option('--clean-assets', is_flag=True, help='Reaquire assets.')
 def build(clean_assets):
-    """Render the projects to static files"""
+    """Build images."""
     logger.debug('build subcommand called from cli')
     commands.clean(clean_assets)
     commands.render()
